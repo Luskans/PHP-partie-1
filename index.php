9ff1a4b87feee844d904d789fef3a5a8f5b729ac
Luskans <sylvain.migne@gmail.com>
2023-06-27 15:47:58 +0200

    add exercices p1

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+/////////// EXERCICE 1 ////////////
+
+// $name = "John";
+// echo($name);
+
+/////////// EXERCICE 2 ////////////
+
+// $lastname = "Doe";
+// $firstname = "John";
+// $age = 30;
+// echo "$lastname $firstname $age ";
+
+/////////// EXERCICE 3 ////////////
+
+// $km = 1;
+// echo "$km <br/>";
+// $km = 3;
+// echo "$km <br/>";
+// $km = 125;
+// echo "$km <br/>";
+
+/////////// EXERCICE 4 ////////////
+
+// $string = "String";
+// $int = 10;
+// $float = 10.5;
+// $isBoolean = true;
+// echo "$string <br/> $int <br/> $float <br/> $isBoolean <br/>";
+
+/////////// EXERCICE 5 ////////////
+
+// $nombre = NULL;
+// echo "$nombre <br/>";
+// $nombre = 12;
+// echo "$nombre <br/>";
+
+/////////// EXERCICE 6 ////////////
+
+// $name = "John";
+// echo "Bonjour $name, comment vas tu ? <br/>";
+// echo 'Bonjour '.$name.', '.'comment vas tu ?';
+
+/////////// EXERCICE 7 ////////////
+
+// $lastname = "Doe";
+// $firstname = "John";
+// $age = 30;
+// echo "Bonjour $lastname $firstname, tu as $age ans.";
+
+/////////// EXERCICE 8 ////////////
+
+$var1 = 3 + 4;
+$var2 = 5 * 20;
+$var3 = 45 / 5;
+echo "$var1 <br/> $var2 <br/> $var3";
+
+?>
